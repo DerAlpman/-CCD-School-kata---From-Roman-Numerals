781e28a2601f2b7a5bf46cce11769023b55df1bf
Stefan Alpers <stefan.alpers@gmx.net>
2019-04-06 13:40:01 +0200

    finished kata

---
+++ FromRomanLiterals/FromRomanLiterals/RomanLiteralsParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FromRomanLiterals
+{
+    public static class RomanLiteralsParser
+    {
+        private static Dictionary<string, string> Map = new Dictionary<string, string>()
+        {
+            {"CM", "900"},
+            { "M", "1000"},
+            { "XC", "90"},
+            { "C", "100"},
+            { "XL", "40"},
+            { "L", "50"},
+            { "IX", "9"},
+            { "X", "10"},
+            { "IV", "4"},
+            { "V", "5"},
+            { "I", "1"}
+        };
+
+        public static int ConvertRomanLiteralToInt(string romanLiteral)
+        {
+            int result = 0;
+
+            foreach (var item in Map)
+            {
+                romanLiteral = romanLiteral.Replace(item.Key, item.Value + ";");
+            }
+
+            foreach (var numberString in romanLiteral.Split(';'))
+            {
+                if (!string.IsNullOrWhiteSpace(numberString))
+                {
+                    result += Convert.ToInt32(numberString);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ FromRomanLiterals/FromRomanLiteralsTests/RomanLiteralsParserTests.cs
@@ -0,0 +1,37 @@
+﻿using FromRomanLiterals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FromRomanLiteralsTests
+{
+    [TestClass]
+    public class RomanLiteralsParserTests
+    {
+        [TestMethod]
+        [DataRow("I", 1)]
+        [DataRow("II", 2)]
+        [DataRow("IV", 4)]
+        [DataRow("V", 5)]
+        [DataRow("IX", 9)]
+        [DataRow("XLII", 42)]
+        [DataRow("XCIX", 99)]
+        [DataRow("MMXIII", 2013)]
+        public void ParserRomanLiteral_RomanLiteral_CorrectArabianNumber(string romanLiteral, int expectedResult)
+        {
+            #region ARRANGE
+
+            #endregion
+
+            #region ACT
+
+            int result = RomanLiteralsParser.ConvertRomanLiteralToInt(romanLiteral);
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.AreEqual(expectedResult, result);
+
+            #endregion
+        }
+    }
+}
